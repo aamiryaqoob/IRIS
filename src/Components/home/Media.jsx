44985a201804fce4a56132662f49929483b88943
aamiryaqoob <aamiryaqoob965@gmail.com>
2023-08-18 22:02:54 +0500

    Updated

--- src/Components/home/Media.jsx
+++ src/Components/home/Media.jsx
@@ -2,19 +2,24 @@
 import React from "react";
 import styles from "./Media.module.css";
 import dark from "./MediaDark.module.css";
-import Image from "next/image";
 import { useSelector } from "react-redux";
-import { BigPlayButton, Player } from "video-react";
 
 const Media = () => {
   const theme = useSelector((state) => state.theme.toggle);
+  
   return (
     <section className={theme ? dark.media_section : styles.media_section}>
       <div className={styles.container}>
         <div className={styles.img_div}>
-          <Player height={"20rem"} poster="/images/mediaimg.png" src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4">
-            <BigPlayButton position="center" />
-          </Player>
+          <video
+            autoPlay
+            loop
+            muted  // Adding muted attribute to improve autoplay compatibility
+            className={styles.video}
+          >
+            <source src="/video.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
         </div>
       </div>
     </section>
